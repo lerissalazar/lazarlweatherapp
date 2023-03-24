eb4b655945fc79a310f2360acdffa76cb1b2904f
Lerissa Lazar <lerissa.lazar12@gmail.com>
2023-03-24 14:57:49 -0700

    link update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 // peer reviewd by Jovann : app brings in data overall look looks awesome data displays but there are no icons for me to see which I think should of been added. I like the responsiveness more time and this app would of been working great.
 
+//https://lerissasweatherappredo.azurewebsites.net/
+
+//https://github.com/lerissalazar/lazarlweatherapp.git
+
 import './App.css';
 import MainComponent from './component/MainComponent';
 import 'bootstrap/dist/css/bootstrap.min.css'
